9deedfd4bcc96dda20dddcccfada13ca9c257c6d
harun <harun.khairy@gmail.com>
2020-04-20 16:48:50 +0700

    gitignore-working-again

--- controllers/fotoControllers.js
+++ controllers/fotoControllers.js
@@ -1,6 +1,7 @@
 const { db } = require('../connection')
 const { uploader } = require('../helper/uploader')
 const fs = require('fs')
+const crypto = require('crypto')
 
 module.exports = {
     postfoto: (req, res) => {
@@ -46,5 +47,86 @@ module.exports = {
             if (err1) return res.status(500).send(err1)
             return res.status(200).send(result1)
         })
+    },
+
+    deletefoto: (req, res) => {
+        const {id} = req.params
+        var sql = `select * from foto where id=${id}`
+        db.query(sql, (err, result) => {
+            if (err) res.status(500).send(err)
+            if (result.length) {
+                sql = `delete from foto where id=${req.params.id}`
+                db.query(sql, (err, result2) => {
+                    if (err) res.status(500).send(err)
+                    console.log(result2)
+                    if (result[0].imagefoto) {
+                        fs.unlinkSync('./public'+result[0].imagefoto)
+                    }
+                    sql = `select * from foto`
+                    db.query(sql, (err1, result1) => {
+                        if (err1) return res.status(500).send(err1)
+                        return res.status(200).send(result1)
+                    })
+                })
+            } else {
+                return res.status(500).send({message:'nggak ada woy idnya'})
+            }
+        })
+    },
+
+    editfoto:(req, res) => {
+        const {id} = req.params
+        var sql = `select * from foto where id=${id}`
+        db.query(sql, (err, result) => {
+            if (err) res.status(500).send(err)
+            if (result.length) {
+                try {
+                    const path ='/foto'//ini terserah
+                    const upload = uploader(path,'TES').fields([{ name: 'image'}])
+                    upload(req, res, (err) => {
+                        if (err) {
+                            return res.status(500).json({ message: 'Upload post picture failed !', error: err.message });
+                        }
+                        console.log('lewat')
+                        const { image } = req.files;
+                        const imagePath = image ? path + '/' + image[0].filename : null;
+                        const data = JSON.parse(req.body.data);
+                        if(imagePath) {
+                            data.imagefoto = imagePath;
+                        }
+                        sql = `Update foto set ? where id = ${id};`
+                        db.query(sql, data, (err1,result1) => {
+                            if (err1) {
+                                if (imagePath) {
+                                    fs.unlinkSync('./public' + imagePath);
+                                }
+                                return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err1.message });
+                            }
+
+                            if (imagePath) {//hapus foto lama
+                                if (result[0].imagefoto) {
+                                    fs.unlinkSync('./public' + result[0].imagefoto);
+                                }
+                            }
+                            sql = `select * from foto`
+                            db.query(sql, (err1, result2) => {
+                                if (err1) return res.status(500).send(err1)
+                                return res.status(200).send(result2)
+                            })
+                        })
+                    })
+                } 
+                catch (error) {
+                }
+            } else {
+                return res.status(500).send({message:'nggak ada woy idnya'})
+            }
+        })
+    },
+
+    encryptkata: (req, res) => {
+        const {kata} = req.query
+        const kataencript = crypto.createHmac('sha256','puripuri').update(kata).digest('hex')
+        res.send(`<h1>${kataencript} dari ${kata} dengan panjang length =${kataencript.length} </h1>`)
     }
 }
--- routers/fotoRouters.js
+++ routers/fotoRouters.js
@@ -5,5 +5,8 @@ const router = express.Router()
 
 router.get('/foto', fotoControllers.getfoto)
 router.post('/foto', fotoControllers.postfoto)
+router.delete('/foto/:id', fotoControllers.deletefoto)
+router.put('/foto/:id', fotoControllers.editfoto)
+router.get('/kataenc', fotoControllers.encryptkata)
 
 module.exports = router
